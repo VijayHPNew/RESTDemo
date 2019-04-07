6b48fb5cbc39be930a80257fe22638261b04a505
Vijay Bharathiar <vijayragul@yahoo.com>
2019-04-07 00:30:33 -0400

    Added a method in HelloWorldController to display text passed as argument (Path Variable)

--- src/main/java/com/loyaltyone/demo/controller/HelloWorldController.java
+++ src/main/java/com/loyaltyone/demo/controller/HelloWorldController.java
@@ -1,15 +1,27 @@
 package com.loyaltyone.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping("/helloworld")
+	@RequestMapping("/displayText")
 	public String welcomeMessage() {
 
 		return "Hello World!!!";
+	}
+
+	@RequestMapping("/displayText/{strText}")
+		public String displayText(@PathVariable String strText) {
+
+			return strText;
 	}
 
 }
